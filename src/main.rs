2d361df25b7ec1d26499401ff7a62274d591c735
Jacob Stegemann <jacob-stegemann@gmx.de>
2022-07-02 00:28:02 +0200

    Implement prime counting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+const UPPER_BOUND: u32 = 100000000;
+
 fn main() {
-    println!("Hello, world!");
+    let mut primes: Vec<u32> = vec![2];
+
+    for p in (3..UPPER_BOUND).step_by(2) {
+        for q in &primes {
+            if q * q > p {
+                primes.push(p);
+                break;
+            }
+
+            if p % q == 0 {
+                break;
+            }
+        }
+    }
+
+    println!(
+        "Found {count} primes below {UPPER_BOUND}",
+        count = primes.len()
+    );
 }
